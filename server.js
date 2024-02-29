2ce41a7cafaf4a6c64e915c623f7ac57fed6b93e
Jirinmas Siripakornchai <naniko.study@gmail.com>
2024-02-29 18:05:48 +0700

    edit server.js

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ dotenv.config({path:'./config/config.env'});
 connectDB();
 
 //Route files
-const hospitals = require('./routes/hospitals');
+const providers = require('./routes/providers');
 const auth = require('./routes/auth');
-const appointments = require('./routes/appointments');
+const bookings = require('./routes/bookings');
 
 const app = express();
 
@@ -22,9 +22,9 @@ app.use(express.json());
 //Cookie parser
 app.use(cookieParser());
 
-app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/providers', providers);
 app.use('/api/v1/auth', auth);
-app.use('/api/v1/appointments', appointments);
+app.use('/api/v1/bookings', bookings);
 
 const PORT=process.env.PORT || 5000;
 
